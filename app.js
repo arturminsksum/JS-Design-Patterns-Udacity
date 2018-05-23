ae9166f56bdc036cd2a84dc5ef79b512636e337e
Artur_Kazlouski <Artur_Kazlouski@epam.com>
2018-05-23 16:02:41 +0300

    interaction between model and view throug controller

--- app.js
+++ app.js
@@ -5,32 +5,39 @@ const model = {
       name: 'Mickey',
       imageUrl:
         'https://farm2.staticflickr.com/1126/625069434_db86b67df8_q.jpg',
-      counter: 0,
+      counter: 0
     },
     {
       id: '1',
       name: 'Max',
       imageUrl:
         'https://farm3.staticflickr.com/2298/2290467335_89067c7b51_q.jpg',
-      counter: 0,
+      counter: 0
     },
     {
       id: '2',
       name: 'Orange',
       imageUrl:
         'https://farm2.staticflickr.com/1276/624215185_6ee2b029b5_q.jpg',
-      counter: 0,
+      counter: 0
     },
-  ],
+    {
+      id: '3',
+      name: 'Ruby',
+      imageUrl:
+        'https://farm3.staticflickr.com/2406/2177097057_df86bce3f8_q.jpg',
+      counter: 0
+    }
+  ]
 };
 
-const app = document.getElementById('app');
+const app = document.getElementById('list');
 
 const view = {
   displayList() {
     const listUl = document.createElement('ul');
 
-    model.cats.forEach(cat => {
+    controller.getCats().forEach(cat => {
       const listLi = document.createElement('li');
       listLi.textContent = cat.name;
       listLi.addEventListener('click', () => {
@@ -41,55 +48,42 @@ const view = {
 
     app.appendChild(listUl);
   },
-  displayCats(cat = model.cats[0]) {
-    const figure = document.createElement('figure');
-    figure.id = cat.id;
-
-    const image = document.createElement('img');
-    image.src = cat.imageUrl;
-    image.addEventListener('click', event => {
-      controller.changeCount(event.target);
-    });
-
-    const figcaption = document.createElement('figcaption');
-
-    const name = document.createElement('p');
-    name.textContent = cat.name;
-
-    const counter = document.createElement('i');
-    counter.textContent = cat.counter;
-
-    figcaption.appendChild(name);
-    figcaption.appendChild(counter);
-
-    figure.appendChild(image);
-    figure.appendChild(figcaption);
-    app.appendChild(figure);
-  },
-};
-
-const controller = {
-  chooseCat(cat) {
+  displayCat(cat) {
     const figure = document.querySelector('figure');
     figure.id = cat.id;
 
     const image = document.querySelector('img');
     image.src = cat.imageUrl;
+    image.addEventListener('click', controller.changeCount);
+
+    const figcaption = document.querySelector('figcaption');
 
     const name = document.querySelector('p');
     name.textContent = cat.name;
 
     const counter = document.querySelector('i');
     counter.textContent = cat.counter;
+  }
+};
+
+const controller = {
+  getCats() {
+    return model.cats;
   },
-  changeCount(target) {
-    const targetId = target.parentNode.id;
+  chooseCat(cat) {
+    view.displayCat(cat);
+  },
+  changeCount(event) {
+    const targetId = event.target.parentNode.id;
     const cat = model.cats.find(cat => cat.id === targetId);
     cat.counter++;
     const counter = document.querySelector('i');
     counter.textContent = cat.counter;
   },
+
+  init() {
+    view.displayList();
+  }
 };
 
-view.displayList();
-view.displayCats();
+controller.init();
